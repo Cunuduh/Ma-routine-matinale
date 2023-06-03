cb2ab7be55ffb6099ec57420427160e2c5a016dc
Cunuduh <61949635+Cunuduh@users.noreply.github.com>
2023-06-02 20:04:01 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,1 @@
-const password = "(1, 2)"
-let proompt = ""
-do {
-    proompt = window.prompt("Résolvez ce système linéaire pour trouver le mot de passe: \n 2𝔁 + 3𝔂 = 8 \n 3𝔁 + 2𝔂 = 7 \n Entrez le mot de passe sous la forme de (𝔁, 𝔂):")
-} while (!proompt)
 
-if (proompt.trim() === password) {
-    alert("Laissez-les entrer")
-} else {
-    alert("Écoutez en cours de mathématiques au lieu de jouer à Valorant")
-    window.location.href = "https://www.youtube.com/watch?v=mxeo3zaf0Vo"
-}
